61da4512f7956aaa43fddf409db0e8b0f70d08c6
megnicholas <megnicholas@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-27 15:41:55 +0000

    tagging version 4.0.9
    git-svn-id: https://plugins.svn.wordpress.org/clean-and-simple-contact-form-by-meg-nicholas/tags/4.0.9@718932 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- class.cff_settings.php
+++ class.cff_settings.php
@@ -40,15 +40,24 @@ class cff_settings
 	    <?php screen_icon(); ?>
 	    <h2> Clean and Simple Contact Form Settings</h2>
         <hr/>
+        <p>You are using version <?php echo CFF_VERSION_NUM;?></p>
+        <p>If you find this plugin useful please consider 
+            <a  target="_blank"
+                href="http://wordpress.org/support/view/plugin-reviews/<?php echo CFF_PLUGIN_NAME; ?>">
+                leaving a review
+                        </a>
+            . Thank you!
+        </p>
 	    <form method="post" action="options.php">
-	        <?php submit_button(); ?>
-	        <?php
+	    <?
+        submit_button(); 
 
-        // This prints out all hidden setting fields
+        /* This prints out all hidden setting fields*/
         settings_fields('test_option_group');
         do_settings_sections('contact-form-settings');
-?>
-	        <?php submit_button(); ?>
+        
+        submit_button();
+        ?>
 	    </form>
 	</div>
 	<?php
@@ -92,6 +101,18 @@ class cff_settings
             $this,
             'print_section_info_message'
         ) , 'contact-form-settings');
+        add_settings_field('recipient_email', 'Recipient Email : ', array(
+            $this,
+            'create_fields'
+        ) , 'contact-form-settings', 'section_message', array(
+            'recipient_email'
+        ));
+        add_settings_field('subject', 'Email Subject: ', array(
+            $this,
+            'create_fields'
+        ) , 'contact-form-settings', 'section_message', array(
+            'subject'
+        ));
         add_settings_field('message', 'Message : ', array(
             $this,
             'create_fields'
@@ -132,30 +153,60 @@ class cff_settings
     {
         $options = get_option(CFF_OPTIONS_KEY);
         
+        //use_recaptcha
         if (isset($input['use_recaptcha'])) 
-        {
             $options['use_recaptcha'] = true;
-        }
         else
-        {
             unset($options['use_recaptcha']);
-        }
         
+        //recaptcha theme
         if (isset($input['theme'])) $options['theme'] = filter_var($input['theme'], FILTER_SANITIZE_STRING);
         
+        //recaptcha_public_key
         if (isset($input['recaptcha_public_key'])) $options['recaptcha_public_key'] = filter_var($input['recaptcha_public_key'], FILTER_SANITIZE_STRING);
         
+        //recaptcha_private_key
         if (isset($input['recaptcha_private_key'])) $options['recaptcha_private_key'] = filter_var($input['recaptcha_private_key'], FILTER_SANITIZE_STRING);
+        
+        //sent_message_heading
         $options['sent_message_heading'] = filter_var($input['sent_message_heading'], FILTER_SANITIZE_STRING);
+        
+        //sent_message_body
         $options['sent_message_body'] = filter_var($input['sent_message_body'], FILTER_SANITIZE_STRING);
+        
+        //message
         $options['message'] = filter_var($input['message'], FILTER_SANITIZE_STRING);
         
-        if (isset($input['load_stylesheet'])) $options['load_stylesheet'] = true;
-        else $options['load_stylesheet'] = false;
+        //load_stylesheet
+        if (isset($input['load_stylesheet'])) 
+            $options['load_stylesheet'] = true;
+        else 
+            $options['load_stylesheet'] = false;
         
-        if (isset($input['use_client_validation'])) $options['use_client_validation'] = true;
-        else $options['use_client_validation'] = false;
+        //use_client_validation
+        if (isset($input['use_client_validation'])) 
+            $options['use_client_validation'] = true;
+        else 
+            $options['use_client_validation'] = false;
+        
+        //recipient_email
+        if (!filter_var($input['recipient_email'], FILTER_VALIDATE_EMAIL)) {
+            unset($options['recipient_email']);
+        }
+        else {
+            $options['recipient_email']=$input['recipient_email'];
+        }
+        
+        //subject
+        $options['subject'] = trim(filter_var($input['subject'], FILTER_SANITIZE_STRING));
+        if ( empty($options['subject']) ) {
+            unset($options['subject']);
+        }
+        
+        //update the options
         update_option(CFF_OPTIONS_KEY, $options);
+            
+                
         
         return $input;
     }
@@ -199,6 +250,12 @@ class cff_settings
         case 'recaptcha_private_key':
             $disabled = cff_PluginSettings::UseRecaptcha() == false ? "disabled" : "";
 ?><input <?php echo $disabled; ?> type="text" size="60" id="recaptcha_private_key" name="array_key[recaptcha_private_key]" value="<?=cff_PluginSettings::PrivateKey(); ?>" /><?php
+        break;
+        case 'recipient_email':
+?><input type="text" size="60" id="recipient_email" name="array_key[recipient_email]" value="<?=cff_PluginSettings::RecipientEmail(); ?>" /><?php
+        break;
+        case 'subject':
+?><input type="text" size="60" id="subject" name="array_key[subject]" value="<?=cff_PluginSettings::Subject(); ?>" /><?php
         break;
         case 'sent_message_heading':
 ?><input type="text" size="60" id="sent_message_heading" name="array_key[sent_message_heading]" value="<?=cff_PluginSettings::SentMessageHeading(); ?>" /><?php
