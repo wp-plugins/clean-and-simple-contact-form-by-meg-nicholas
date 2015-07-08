8bd7ee8ad039037d4c8d78239ba45f83d6eca287
megnicholas <megnicholas@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-08 12:19:24 +0000

    tagging 4.5.0
    git-svn-id: https://plugins.svn.wordpress.org/clean-and-simple-contact-form-by-meg-nicholas/tags/4.5.0@1194730 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/jquery.validate.contact.form.js
+++ js/jquery.validate.contact.form.js
@@ -1,95 +1,94 @@
-jQuery(document).ready(function($) {
-    
+jQuery(document).ready(function ($) {
+
     var $div = $("#cscf");
-    
+
     var $form = $div.find("#frmCSCF");
-    
-    $form.find("#recaptcha_response_field").focus(function() {
+
+    $form.find("#recaptcha_response_field").focus(function () {
 
         $errele = $form.find("div[for='cscf_recaptcha']");
         $errele.html('');
-        
+
     });
-    
-    $form.validate({ 
+
+    $form.validate({
 
         errorElement: "span",
 
-        highlight: function(label, errorClass, validClass) {
-                               $(label).closest('.form-group').removeClass('has-success').addClass('has-error'); 
-                               $(label).closest('.control-group').removeClass('success').addClass('error'); // support for bootstrap 2
-                               
-                       },
-        unhighlight: function(label, errorClass, validClass) {
-               $(label).closest('.form-group').removeClass('has-error').addClass('has-success');
-               $(label).closest('.control-group').removeClass('error').addClass('success'); // support for bootstrap 2
+        highlight: function (label, errorClass, validClass) {
+            $(label).closest('.form-group').removeClass('has-success').addClass('has-error');
+            $(label).closest('.control-group').removeClass('success').addClass('error'); // support for bootstrap 2
+
+        },
+        unhighlight: function (label, errorClass, validClass) {
+            $(label).closest('.form-group').removeClass('has-error').addClass('has-success');
+            $(label).closest('.control-group').removeClass('error').addClass('success'); // support for bootstrap 2
         }
-     });
-        
+    });
+
     $form.submit(function (event) {
 
-          $button = $(this).find("button");
-          $button.attr("disabled","disabled");
+        $button = $(this).find("button");
+        $button.attr("disabled", "disabled");
 
-          event.preventDefault();
+        event.preventDefault();
 
-          if ($form.validate().valid() ) {
+        if ($form.validate().valid()) {
 
-              $.ajax({
-              type : "post",
-              dataType : "json",
-              cache: false,
-              url : cscfvars.ajaxurl,
-              data : $($form).serialize() + "&action=cscf-submitform", 
-              success: function(response,strText) {
-                                                      if (response.valid === true) { 
-                                                          //show sent message div
-                                                          $formdiv=$div.find(".cscfForm");
-                                                          $formdiv.css('display','none');
-                                                          $messagediv=$div.find(".cscfMessageSent");
-                                                          if (response.sent === false ) {
-                                                              $messagediv=$div.find(".cscfMessageNotSent");
-                                                          }
+            $.ajax({
+                type: "post",
+                dataType: "json",
+                cache: false,
+                url: cscfvars.ajaxurl,
+                data: $($form).serialize() + "&action=cscf-submitform",
+                success: function (response, strText) {
+                    if (response.valid === true) {
+                        //show sent message div
+                        $formdiv = $div.find(".cscfForm");
+                        $formdiv.css('display', 'none');
+                        $messagediv = $div.find(".cscfMessageSent");
+                        if (response.sent === false) {
+                            $messagediv = $div.find(".cscfMessageNotSent");
+                        }
 
-                                                          $messagediv.css('display','block');
+                        $messagediv.css('display', 'block');
 
-                                                          if ( isScrolledIntoView($div) == false) {
-                                                              scrollTo($div.selector);
-                                                          }
-                                                      }
+                        if (isScrolledIntoView($div) == false) {
+                            jQuery('html,body')
+                                .animate({
+                                    scrollTop: jQuery($div.selector)
+                                        .offset().top
+                                }, 'slow');
+                        }
+                    }
 
-                                                      else { 
-                                                          $.each(response.errorlist, function(name, value) {
-                                                              $errele = $form.find("div[for='cscf_" + name +"']");
-                                                              $errele.html(value);
-                                                              $errele.closest('.form-group').removeClass('has-success').addClass('has-error');
-                                                              $errele.closest('.control-group').removeClass('success').addClass('error'); // support for bootstrap 2
-                                                          });
-                                                      }
-                                                   },
-              error: function(XMLHttpRequest, textStatus, errorThrown) { 
-                      if (window.console) {
-                          console.log("Status: " + textStatus + "Error: " + errorThrown + "Response: " + XMLHttpRequest.responseText);
-                      }
+                    else {
+                        $.each(response.errorlist, function (name, value) {
+                            $errele = $form.find("div[for='cscf_" + name + "']");
+                            $errele.html(value);
+                            $errele.closest('.form-group').removeClass('has-success').addClass('has-error');
+                            $errele.closest('.control-group').removeClass('success').addClass('error'); // support for bootstrap 2
+                        });
+                    }
+                },
+                error: function (XMLHttpRequest, textStatus, errorThrown) {
+                    if (window.console) {
+                        console.log("Status: " + textStatus + "Error: " + errorThrown + "Response: " + XMLHttpRequest.responseText);
+                    }
 
-                  } 													
+                }
 
-              }); 
+            });
 
-          };
-          $button.removeAttr("disabled");
-      });         
-        
+        }
+        ;
+        $button.removeAttr("disabled");
+    });
+
 });
 
-function scrollTo(id)
-{
-  // Scroll
-  jQuery('html,body').animate({scrollTop: jQuery(id).offset().top},'slow');
-}
 
-function isScrolledIntoView(elem)
-{
+function isScrolledIntoView(elem) {
     var docViewTop = jQuery(window).scrollTop();
     var docViewBottom = docViewTop + jQuery(window).height();
 
